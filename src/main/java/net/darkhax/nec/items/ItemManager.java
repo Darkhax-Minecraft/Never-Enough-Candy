0a3188ba13e595e5ec78797dbeb07e8c0b24e3af
Darkhax <darklime@live.ca>
2015-10-09 03:27:16 -0600

    Added in all of the old items from the Haunted mod.

--- src/main/java/net/darkhax/nec/items/ItemManager.java
+++ src/main/java/net/darkhax/nec/items/ItemManager.java
@@ -3,11 +3,31 @@ package net.darkhax.nec.items;
 import java.util.ArrayList;
 import java.util.List;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.darkhax.nec.handler.ConfigurationHandler;
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.EntitySpider;
+import net.minecraft.entity.monster.EntityWitch;
+import net.minecraft.entity.passive.EntityBat;
+
 public class ItemManager {
     
     public static List<CandyType> candies = new ArrayList<CandyType>();
     
     public ItemManager() {
-    
+        
+        candies.add(new CandyType("batty", 2, 0.05f, EntityBat.class));
+        candies.add(new CandyType("witch", 2, 0.05f, EntityWitch.class));
+        candies.add(new CandyType("spider", 2, 0.05f, EntitySpider.class));
+        candies.add(new CandyType("enderpop", 2, 0.05f, EntityEnderman.class));
+        candies.add(new CandyType("skull", 2, 0.05f, EntitySkeleton.class, true));
+        
+        for (CandyType type : candies) {
+            
+            ConfigurationHandler.syncType(type);
+            type.item = new ItemCandy(type);
+            GameRegistry.registerItem(type.item, type.name);
+        }
     }
 }
