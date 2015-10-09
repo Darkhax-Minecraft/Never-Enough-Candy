b93ec6f5d9ff2b518b0d284dc351ea0cdf0911fd
Darkhax <darklime@live.ca>
2015-10-09 09:25:40 -0600

    Added in loot.

---
+++ src/main/java/net/darkhax/nec/handler/LootHandler.java
@@ -0,0 +1,29 @@
+package net.darkhax.nec.handler;
+
+import java.util.Map;
+
+import net.darkhax.nec.items.CandyType;
+import net.darkhax.nec.items.ItemManager;
+import net.darkhax.nec.items.ItemPlayerCookie;
+import net.darkhax.nec.util.Utilities;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraftforge.common.ChestGenHooks;
+
+public class LootHandler {
+    
+    public LootHandler() {
+        
+        String[] locations = new String[] {"mineshaftCorridor", "pyramidDesertyChest", "pyramidJungleChest", "pyramidJungleDispenser", "strongholdCorridor", "strongholdLibrary", "strongholdCrossing", "villageBlacksmith", "bonusChest", "dungeonChest"};
+        
+        for (String location : locations) {
+            
+            for (CandyType type : ItemManager.candies)
+                ChestGenHooks.addItem(location, new WeightedRandomChestContent(new ItemStack(type.item), 5, 1, 3));
+            
+            for (Map.Entry<String, ItemPlayerCookie> entry : ItemManager.cookies.entrySet())
+                ChestGenHooks.addItem(location, new WeightedRandomChestContent(new ItemStack(entry.getValue()), 2, 1, 1));
+        }
+    }
+}
