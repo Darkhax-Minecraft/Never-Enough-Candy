0deb7b50c3dcca1fb06c8ade4b0d58c37d182773
Darkhax <darklime@live.ca>
2015-10-09 08:52:35 -0600

    Utilities.

--- src/main/java/net/darkhax/nec/util/Utilities.java
+++ src/main/java/net/darkhax/nec/util/Utilities.java
@@ -1,13 +1,40 @@
 package net.darkhax.nec.util;
 
+import java.awt.Color;
 import java.util.List;
 
 import org.apache.commons.lang3.SystemUtils;
 import org.apache.commons.lang3.text.WordUtils;
 
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.EnumChatFormatting;
 
 public class Utilities {
+    
+    public static final int POTION_COUNT = Potion.potionTypes.length;
+    
+    public static Potion getRandomPotionEffect () {
+        
+        Potion potion = null;
+        
+        while (potion == null)
+            potion = Potion.potionTypes[Constants.RANDOM.nextInt(POTION_COUNT)];
+            
+        return potion;
+    }
+    
+    public static int getRandomColor () {
+        
+        Color color = new Color(Constants.RANDOM.nextFloat(), Constants.RANDOM.nextFloat(), Constants.RANDOM.nextFloat());
+        return color.getRGB();
+    }
+    
+    public static boolean isSameEntity (String type, EntityLivingBase entity) {
+        
+        return (EntityList.getEntityString(entity).equalsIgnoreCase(type));
+    }
     
     public static List<String> generateTooltip (String string, List<String> list) {
         
