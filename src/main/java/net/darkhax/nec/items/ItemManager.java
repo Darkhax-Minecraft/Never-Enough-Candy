b391f9df16bef2c6506d93bf004e53c5cb316863
Darkhax <darklime@live.ca>
2015-10-09 02:09:25 -0600

    Added the basics for new candies.

---
+++ src/main/java/net/darkhax/nec/items/ItemManager.java
@@ -0,0 +1,13 @@
+package net.darkhax.nec.items;
+
+import java.util.HashMap;
+
+public class ItemManager {
+
+	public HashMap<String, CandyType> candies = new HashMap<String, CandyType>();
+	
+	public ItemManager() {
+		
+		
+	}
+}
