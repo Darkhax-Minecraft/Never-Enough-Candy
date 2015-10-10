ae968add0cfa8f0660bc4a396d094870dd9fc653
Darkhax <darklime@live.ca>
2015-10-09 21:00:48 -0600

    Tweaked the drop handler to work with beans.

--- src/main/java/net/darkhax/nec/handler/ForgeEventHandler.java
+++ src/main/java/net/darkhax/nec/handler/ForgeEventHandler.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.darkhax.nec.items.CandyType;
+import net.darkhax.nec.items.ItemBeans;
 import net.darkhax.nec.items.ItemManager;
 import net.darkhax.nec.items.ItemPlayerCookie;
 import net.darkhax.nec.util.Utilities;
@@ -17,10 +18,16 @@ public class ForgeEventHandler {
     @SubscribeEvent
     public void onDropsProcessed (LivingDropsEvent event) {
         
+        if (Utilities.tryPercentage(0.01f))
+            event.drops.add(new EntityItem(event.entityLiving.worldObj, event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ, ItemBeans.generateBean()));
+        
         if (event.entityLiving instanceof EntityPlayerMP)
             for (Map.Entry<String, ItemPlayerCookie> entry : ItemManager.cookies.entrySet())
-                if (((EntityPlayerMP) event.entityLiving).getCommandSenderName().equalsIgnoreCase(entry.getKey()))
+                if (((EntityPlayerMP) event.entityLiving).getCommandSenderName().equalsIgnoreCase(entry.getKey())) {
+                    
                     event.drops.add(new EntityItem(event.entityLiving.worldObj, event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ, new ItemStack(entry.getValue())));
+                    return;
+                }
                     
         for (CandyType type : ItemManager.candies)
             if (type.type.equalsIgnoreCase("any") || Utilities.isSameEntity(type.type, event.entityLiving))
