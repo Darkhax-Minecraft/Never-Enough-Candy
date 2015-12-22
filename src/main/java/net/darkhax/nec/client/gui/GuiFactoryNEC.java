e4abd2c296f6ec89afa9e466b077ec9c02a028ea
GirafiStudios <girafi0@gmail.com>
2015-12-22 19:57:46 +0100

    Ported to 1.8.X *Meaning this version works both on 1.8 & 1.8.8

--- src/main/java/net/darkhax/nec/client/gui/GuiFactoryNEC.java
+++ src/main/java/net/darkhax/nec/client/gui/GuiFactoryNEC.java
@@ -1,10 +1,10 @@
 package net.darkhax.nec.client.gui;
 
-import java.util.Set;
-
-import cpw.mods.fml.client.IModGuiFactory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+import java.util.Set;
 
 public class GuiFactoryNEC implements IModGuiFactory {
     
--- src/main/java/net/darkhax/nec/items/ItemCandyApple.java
+++ src/main/java/net/darkhax/nec/items/ItemCandyApple.java
@@ -1,9 +1,5 @@
 package net.darkhax.nec.items;
 
-import java.util.List;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.darkhax.nec.util.Utilities;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -13,6 +9,10 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 public class ItemCandyApple extends ItemFood {
     
@@ -21,7 +21,6 @@ public class ItemCandyApple extends ItemFood {
         super(15, 1.2F, false);
         this.setUnlocalizedName("nec.apple");
         this.setCreativeTab(CreativeTabs.tabFood);
-        this.setTextureName("nec:golden_caramel_apple");
         this.maxStackSize = 1;
         this.setAlwaysEdible();
     }
@@ -40,8 +39,7 @@ public class ItemCandyApple extends ItemFood {
     
     @Override
     @SideOnly(Side.CLIENT)
-    public boolean hasEffect (ItemStack par1ItemStack, int pass) {
-        
+    public boolean hasEffect (ItemStack stack) {
         return true;
     }
     
